c814f92994089e9a9624d9111765be3980fdf4e4
Abed Nganga <ngangaabed@gmail.com>
2024-01-25 22:07:07 +0300

    Added search bar

--- FlavorFiesta/src/screens/HomeScreen.js
+++ FlavorFiesta/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StatusBar, ScrollView, Image } from 'react-native'
+import { View, Text, StatusBar, ScrollView, Image, TextInput } from 'react-native'
 import React from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import {BellIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
@@ -21,7 +21,7 @@ const HomeScreen = () => {
 
         {/* greetings and punchline */}
         <View className="mx-4 space-y-2 mb-2">
-          <Text style={{fontSize: hp(2)}} className="text-neutral-600">Hello, Abed!</Text>
+          <Text style={{fontSize: hp(2.5)}} className="text-neutral-600">Hello, Abed!</Text>
           <View>
             <Text style={{fontSize: hp(3.8)}} className="font-semibold text-neutral-600">Make your own food,</Text>
           </View>
@@ -29,6 +29,19 @@ const HomeScreen = () => {
             stay at <Text className="text-amber-400">home</Text>
           </Text>
       </View>
+
+      {/* search bar */}
+        <View className="mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]">
+          <TextInput
+            placeholder='Search any recipe'
+            placeholderTextColor={'gray'}
+            style={{fontSize: hp(2)}}
+            className="flex-1 text-base mb-1 pl-3 tracking-wider"
+          />
+          <View className="bg-white rounded-full p-3">
+            <MagnifyingGlassIcon size={hp(2.5)} strokeWidth={1} color="gray" />
+          </View>
+        </View>
       </ScrollView>
     </View>
   )
