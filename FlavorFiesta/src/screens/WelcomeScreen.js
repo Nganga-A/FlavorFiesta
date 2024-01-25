704e0491c75747f30a6b18ce298e6345fc5d8fda
Abed Nganga <ngangaabed@gmail.com>
2024-01-25 20:50:15 +0300

    Initial welcome page structure

--- FlavorFiesta/src/screens/WelcomeScreen.js
+++ FlavorFiesta/src/screens/WelcomeScreen.js
@@ -1,11 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text,Image, StatusBar } from 'react-native'
 import React from 'react'
 
 export default function WelcomeScreen() {
-  return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    return (
+    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
+        <StatusBar style="light"/>
+
+        {/* logo image with rings */}
+        <View className="bg-white/20 rounded-full">
+        <View className="bg-white/20 rounded-full">
+            <Image source={require('../../assets/images/welcome.png')}
+                style={{width:200, height: 200}} />
+        </View>
+        </View>
+
+        {/* tittle and punchline */}
+        <View className="flex items-center space-y-2">
+            <Text className="font-bold text-6xl text-white tracking-widest">
+                Foody
+            </Text>
+            <Text className="font-medium text-white tracking-widest text-lg">
+                Food is always right
+            </Text>
+        </View>
+
+
+
     </View>
-  )
+    )
 }
 
