c11f3283b5dcea3c809ac65ce3d3dd0cd4685611
Abed Nganga <ngangaabed@gmail.com>
2024-01-25 13:39:40 +0300

    Added a welcome screen

--- FlavorFiesta/src/navigation/index.js
+++ FlavorFiesta/src/navigation/index.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
 
 
 
@@ -12,6 +13,7 @@ function AppNavigation() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Welcome' screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
